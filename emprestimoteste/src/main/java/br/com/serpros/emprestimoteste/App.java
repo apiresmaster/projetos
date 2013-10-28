3bf123c58c64514d8a33f4e0285243e960f64290
alexandre.pires <alexandre.pires@Ubuntu12-disi09>
2013-10-28 13:49:45 -0200

    Testar o uso do mapeamento das tabelas beneficiarios, participantes_cd, lanfolext e lanrubext para mapeamento ORM.

---
+++ emprestimoteste/src/main/java/br/com/serpros/emprestimoteste/App.java
@@ -0,0 +1,13 @@
+package br.com.serpros.emprestimoteste;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
